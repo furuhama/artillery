f77b9a6798a195ce7136c4ded0fba345d0269240
furuhama <furuhama@users.noreply.github.com>
2018-06-03 20:16:05 +0900

    Add NOLINT option for long line

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ using std::endl;
 
 int main() {
     cout << "Welcome to Artillery" << endl;
-    cout << "You are in the middle of a war and being charged by thousands of enemies." << endl;
+    cout << "You are in the middle of a war and being charged by thousands of enemies." << endl; // NOLINT
     cout << "You have one cannon, which you can shoot at any angle." << endl;
     cout << "You only have 10 cannonballs for this target." << endl;
     cout << "Let's begin..." << endl;
