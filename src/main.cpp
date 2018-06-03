504393e22de612be3159ffd7769dd3be31c9bada
furuhama <furuhama@users.noreply.github.com>
2018-06-03 20:11:29 +0900

    Make whole processes by a simple io way

--- src/main.cpp
+++ src/main.cpp
@@ -8,5 +8,13 @@ using std::cin;
 using std::endl;
 
 int main() {
-    cout << "test" << endl;
+    cout << "Welcome to Artillery" << endl;
+    cout << "You are in the middle of a war and being charged by thousands of enemies." << endl;
+    cout << "You have one cannon, which you can shoot at any angle." << endl;
+    cout << "You only have 10 cannonballs for this target." << endl;
+    cout << "Let's begin..." << endl;
+    cout << endl;
+    cout << "main process" << endl;
+    int killed = 0;
+    cout << "You killed " << killed << " of the enemy." << endl;
 }
