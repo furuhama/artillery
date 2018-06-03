5e3c4237c29024e2d1c2aa2f4e617c92df74a54b
furuhama <furuhama@users.noreply.github.com>
2018-06-03 20:18:27 +0900

    Separate starting up documents outputs to StartUp()

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,19 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-int main() {
+
+
+void StartUp() {
     cout << "Welcome to Artillery" << endl;
     cout << "You are in the middle of a war and being charged by thousands of enemies." << endl; // NOLINT
     cout << "You have one cannon, which you can shoot at any angle." << endl;
     cout << "You only have 10 cannonballs for this target." << endl;
     cout << "Let's begin..." << endl;
     cout << endl;
+}
+
+int main() {
+    StartUp();
     cout << "main process" << endl;
     int killed = 0;
     cout << "You killed " << killed << " of the enemy." << endl;
